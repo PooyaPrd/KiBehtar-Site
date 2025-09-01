78d357866e0f80ba9d7473c56424a7e367dae933
Pouya-Pourarshad <pouyapourarshad@gmail.com>
2025-09-01 21:21:11 +0330

    huge changes

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ AOS.init({
   duration: 1000, 
   once: true   
 });
+
+const toggle = document.getElementById('toggle');
+
+const toggles = document.querySelectorAll('input[name="toggle"]');
+
+toggles.forEach(toggle => {
+  toggle.addEventListener('change', () => {
+    if (toggle.checked) {
+      if(toggle.value == "a"){
+        
+      }
+    }
+  });
+});
